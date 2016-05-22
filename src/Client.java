fdc557c6063f93caa8c4d5f39d02bc811086eec3
Stefan <stefan3012@hotmail.de>
2016-05-22 12:53:33 +0200

    Klijent - obradjeni neki zahtjevi i odgovori servera

---
+++ src/Client.java
@@ -0,0 +1,201 @@
+import gui.Gui;
+import gui.controller.GameController;
+import gui.controller.LobbyController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+
+public class Client {
+	
+	private Gui gui;
+	private PrintWriter out;
+	private BufferedReader in;
+	private GameController c;
+	private LobbyController l;
+	private LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<String>();
+	
+	private String gameId;
+	private String username;
+	private String whosOnMove;
+	
+	public Client() {
+		gui = Gui.getInstance();
+		l = gui.loadLobbyView(true);
+		c = gui.loadGameView();
+		initialize();
+	}
+	
+	private void initialize() {
+		gameId = "";
+		username = "default";
+		whosOnMove = username;
+		
+		//try(Socket client = new Socket("localhost", 1338)) {
+		try(Socket client = new Socket("hekate.zx.rs", 1338)) {
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			out = new PrintWriter(client.getOutputStream(), true);
+			
+			initGameCallbacks();
+			initLobbyCallbacks();
+			
+			try {
+				in.readLine();
+				@SuppressWarnings("unused")
+				String line;
+				while (true)
+					process(line = in.readLine(), in);
+				
+			} catch (Exception e) { }
+			
+		} catch (IOException e) { 
+			Platform.exit();
+		}
+	}
+
+	private void process(String line, BufferedReader in) {
+		try {
+			if (line.startsWith("PING")) 
+				out.println("PONG");
+			else if (line.startsWith("E_OK")) {
+				queue.add(line);
+				System.out.println(line); // server cesto vraca E_OK pa ga obradjujemo ovde
+			}
+			else if (line.equals("E_USERS:")) {
+				System.out.println(line);
+				LinkedList<String> users = new LinkedList<>();
+				while ((line = in.readLine()).endsWith("\r\n")) {
+					System.out.println(line + " - in while");
+					users.add(line);
+				}
+				l.setPlayers(users);
+				l.addChatInfo(users.get(0));
+			}
+			else if (line.equals("E_GAMES:")) {
+				LinkedList<String> games = new LinkedList<>();
+				while ((line = in.readLine()).endsWith("\r\n"))
+					games.add(line);
+				
+				//l.getGamesList().clear();
+				//l.getGamesList().addAll(games);
+			}
+			else if (line.startsWith("E_TURN")) {
+				whosOnMove = line.substring(line.indexOf(":") + 1);
+				c.setGameInfo(whosOnMove);
+			}
+
+			else if (line.startsWith("E_LOBBY_MESSAGE")) {
+				String poruka = line.substring(line.indexOf(":") + 2);
+				String userSent = poruka.substring(0, poruka.indexOf(":"));
+				Platform.runLater(() -> 
+					l.addChatMessage(userSent, poruka.substring(poruka.indexOf(":") + 2))
+				);	
+			}
+			else if (line.startsWith("E_LOBBY_INFO")) {
+				String poruka = line.substring(line.indexOf(":") + 2);
+				Platform.runLater(() -> 
+					l.addChatInfo(poruka.substring(poruka.indexOf(":") + 2))
+				);	
+			} 
+			else if (line.startsWith("E_MESSAGE")) {
+				String poruka = line.substring(line.indexOf(":") + 2);
+				String userSent = poruka.substring(0, poruka.indexOf(":"));
+				Platform.runLater(() -> 
+					c.addChatMessage(userSent, poruka.substring(poruka.indexOf(":") + 2))
+				);	
+			} 
+			else if (line.startsWith("E_INFO")) {
+				String poruka = line.substring(line.indexOf(":") + 2);
+				Platform.runLater(() -> 
+					l.addChatInfo(poruka.substring(poruka.indexOf(":") + 2))
+				);	
+			}
+			else if (line.startsWith("E_GAME_REQUEST")) {
+				// FIX - treba lista u LobbyViewu
+				// l.getRequestList().add(line.substring(line.indexOf(":") + 2));
+			}
+			else if (line.startsWith("E_GAME_ACCEPTED")) {
+				// FIX - treba lista u LobbyViewu
+				// l.getRequestList().remove(line.substring(line.indexOf(":") + 2));
+				
+			}
+			else if (line.startsWith("E_GAME_DECLINED")) {
+				//						
+			}
+			else if (line.equals("E_INVALID_MOVE"))
+				l.addChatInfo("It's opponenets move."); // da li može doći do ovoga (kako je logika impl)?
+			
+			else if (line.equals("E_GAME_OVER")) {
+				// treba obraditi slučaj kada je korisnik prekinuo igru - onPlayerLeave()
+				// i kada je igra završena pobjedom nekog od takmičara - checkState())
+				// ili eventualno nerješeno
+			}
+			else {
+				queue.add(line);
+				System.out.println(line);
+				//toChatInfo(line);
+			}
+		} catch (IOException e) { }
+	}
+	
+	@SuppressWarnings("unused")
+	private void toChatInfo(String line) {
+		final String fin = line;
+		Platform.runLater(() -> {
+			l.addChatInfo(fin);
+		});
+	}
+	
+	private void initLobbyCallbacks() {
+		l.onLoginButton(() -> {
+			l.setButtonEnabled(false);
+			l.setLoginError("");
+			username = l.getLoginUsername();
+			out.println("LOGIN: " + username);
+			// DELETE LINE
+			System.out.println("LOGIN: " + username);
+			try {
+				String line = queue.take();
+				if (line.startsWith("E_OK"))
+					l.setLoginVisible(false);
+				else if (line.equals("E_USERNAME_TAKEN"))
+					l.setLoginError("Username already taken. ");
+				
+			} catch (Exception e) { }
+			l.setButtonEnabled(true);
+		});
+		
+		l.onChatButton(() -> {
+			out.println("LOBBYMSG: " + l.getChatInput());
+			l.setChatInput("");
+		}); 
+		
+	}
+	private void initGameCallbacks() {
+		c.onLeaveButton(() -> {
+			out.println("LEAVE GAME: " + gameId);
+		}); 
+		
+		c.onChatButton(() -> {
+			out.println("GAMEMSG: " + c.getChatInput());
+			c.setChatInput("");
+		}); 
+		
+		c.setGameInfo(whosOnMove + "'s turn");
+	}
+	
+	public static void main(String[] args) throws IOException {
+		new Thread(() -> {
+			Application.launch(Gui.class, args);
+		}).start();
+		
+		new Client();
+	}
+}
